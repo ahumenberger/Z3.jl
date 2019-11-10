52c99fc8f38ec3643f5c4fb4da159767480dcd5d
Andreas Humenberger <ahumenberger@gmail.com>
2019-11-10 13:06:50 +0100

    More types

--- deps/src/z3.cpp
+++ deps/src/z3.cpp
@@ -4,63 +4,68 @@
 using namespace z3;
 using namespace jlcxx;
 
-#define EXPR_OPCALL(OP) \
-    mod.method(#OP, [](const expr& a, const expr& b){return a OP b;}); \
-    mod.method(#OP, [](const expr& a, int b)        {return a OP b;}); \
-    mod.method(#OP, [](int a, const expr& b)        {return a OP b;});
-#define EXPR_FCALL(FNAME, F) \
+#define EXPR_OPCALL(OP, TYPE) EXPR_NAMED_OPCALL(OP, OP, TYPE)
+#define EXPR_NAMED_OPCALL(FNAME, OP, TYPE) \
+    mod.method(#FNAME, [](const expr& a, const expr& b) {return a OP b;}); \
+    mod.method(#FNAME, [](const expr& a, TYPE b)        {return a OP b;}); \
+    mod.method(#FNAME, [](TYPE a, const expr& b)        {return a OP b;});
+#define EXPR_FCALL(FNAME, F, TYPE) \
     mod.method(#FNAME, [](const expr& a, const expr& b) {return F(a, b);}); \
-    mod.method(#FNAME, [](const expr& a, int b)         {return F(a, b);}); \
-    mod.method(#FNAME, [](int a, const expr& b)         {return F(a, b);});
+    mod.method(#FNAME, [](const expr& a, TYPE b)        {return F(a, b);}); \
+    mod.method(#FNAME, [](TYPE a, const expr& b)        {return F(a, b);});
+
+#define STRING(TYPE) \
+    .method("string", [](TYPE x){ \
+        std::ostringstream stream;       \
+        stream << x;                     \
+        return stream.str();             \
+    })
 
 template<> struct IsBits<check_result> : std::true_type {};
 
 JLCXX_MODULE define_julia_module(jlcxx::Module& mod)
 {
-    mod.add_type<context>("context")
+    mod.add_type<context>("Context")
         .method("real_const", &context::real_const);
 
-    mod.add_type<expr>("expr")
+    mod.add_type<expr>("Expr")
         .constructor<context &>()
         // .constructor<context &, Z3_ast>()
         .constructor<expr const &>()
-        .method("string", [](const expr& x){
-            std::ostringstream stream;
-            stream << x;
-            return stream.str();
-        });
+        STRING(expr const &);
 
-    EXPR_OPCALL(==)
-    EXPR_OPCALL(!=)
-    EXPR_OPCALL(+)
-    EXPR_OPCALL(-)
+    EXPR_OPCALL(+, int)
+    EXPR_OPCALL(-, int)
     mod.method("-", [](const expr& a) {return -a;});
-    EXPR_OPCALL(*)
-    EXPR_OPCALL(/)
-    EXPR_OPCALL(^)
-    EXPR_OPCALL(&&)
-    EXPR_OPCALL(||)
-    mod.method("!", [](const expr& a) {return !a;});
-    EXPR_OPCALL(<=)
-    EXPR_OPCALL(>=)
-    EXPR_OPCALL(<)
-    EXPR_OPCALL(>)
+    EXPR_OPCALL(*, int)
+    EXPR_OPCALL(/, int)
+    EXPR_FCALL(^, pw, int)
+    EXPR_NAMED_OPCALL(or,  ||, bool)
+    EXPR_NAMED_OPCALL(and, &&, bool)
+    mod.method("not", [](const expr& a) {return !a;});
+    EXPR_OPCALL(==, int)
+    EXPR_OPCALL(!=, int)
+    EXPR_OPCALL(<=, int)
+    EXPR_OPCALL(>=, int)
+    EXPR_OPCALL(<,  int)
+    EXPR_OPCALL(>,  int)
 
-    mod.add_bits<check_result>("check_result", jlcxx::julia_type("CppEnum"));
+    mod.add_bits<check_result>("CheckResult", jlcxx::julia_type("CppEnum"));
     mod.set_const("unsat", unsat);
     mod.set_const("sat", sat);
     mod.set_const("unknown", unknown);
 
-    mod.add_type<solver>("solver")
+    mod.add_type<solver>("Solver")
         .constructor<context &>()
         .constructor<context &, char const *>()
         .method("add",      static_cast<void (solver::*)(const expr&)>(&solver::add))
         .method("check",    static_cast<check_result (solver::*)()>(&solver::check))
         .method("get_model", &solver::get_model)
-        .method("unsat_core", &solver::unsat_core);
+        .method("unsat_core", &solver::unsat_core)
+        STRING(solver const &);
 
-    mod.add_type<model>("model")
+    mod.add_type<model>("Model")
         .method("num_consts", &model::num_consts)
-        .method("get_const_decl", &model::get_const_decl);
-
+        .method("get_const_decl", &model::get_const_decl)
+        STRING(model const &);
 }
--- src/Z3.jl
+++ src/Z3.jl
@@ -2,16 +2,37 @@ module Z3
 
 import Libdl
 using CxxWrap
-
 import Base: +, -, *, /, ^, ==, !=, !, <=, >=, <, >
 import Base: string
 
-@wrapmodule(realpath(joinpath(@__DIR__, "..", "deps", "src", "libz3jl." * Libdl.dlext)))
+@wrapmodule(realpath(joinpath(Base.@__DIR__, "..", "deps", "src", "libz3jl." * Libdl.dlext)))
 
 function __init__()
     @initcxx
 end
 
-# Base.show(io::IO, x::expr) = print(io, get_string(x))
+function __should_be_exported(sym::Symbol)
+    s = string(sym)
+    # Private
+    startswith(s, "__")   && return false
+    startswith(s, "#")    && return false
+    isequal(s, "Z3")      && return false
+    isequal(s, "include") && return false
+    isequal(s, "eval")    && return false
+    # Types created by CxxWrap
+    endswith(s, "Allocated") && return false
+    endswith(s, "Ref")       && return false
+    # Enums
+    eval(sym) isa CheckResult && return false
+    # Conflicts
+    isequal(sym, :Expr) && return false
+    true
+end
+
+for name in names(Z3, all=true, imported=false)
+    if __should_be_exported(name)
+        @eval export $name 
+    end
+end
 
 end # module
