5f14a8ade58e5f9258a323484ec7edfaaa379827
Andreas Humenberger <ahumenberger@gmail.com>
2019-11-16 07:59:59 +0100

    Using LibGit2

--- deps/build.jl
+++ deps/build.jl
@@ -1,22 +1,29 @@
+using LibGit2
+using CMake
 using CxxWrap
 
-z3dir = joinpath(pwd(), "z3")
-z3builddir = joinpath(z3dir, "build")
+z3_url = "https://github.com/Z3Prover/z3"
+z3_version = "4.8.6"
+
+z3_dir = joinpath(pwd(), "z3")
+z3_builddir = joinpath(z3_dir, "build")
 srcdir = joinpath(pwd(), "src")
 
-try
-    run(`git clone --branch z3-4.8.6 https://github.com/Z3Prover/z3.git`)
-catch
+if !isdir(z3_dir)
+    repo = LibGit2.clone(z3_url, z3_dir)
+    hash = LibGit2.GitHash(repo, "refs/tags/z3-$z3_version")
+    LibGit2.checkout!(repo, string(hash))
 end
-if !isdir(z3builddir)
-    mkdir(z3builddir)
+
+if !isdir(z3_builddir)
+    mkdir(z3_builddir)
 end
-cd(z3builddir)
-run(`cmake $z3dir`)
+cd(z3_builddir)
+run(`cmake $z3_dir`)
 run(`make`)
 
 JlCxx_dir = joinpath(dirname(CxxWrap.jlcxx_path), "cmake", "JlCxx")
 
 cd(srcdir)
-run(`cmake -DJlCxx_DIR=$JlCxx_dir -DZ3_DIR=$z3builddir`)
+run(`$cmake -DJlCxx_DIR=$JlCxx_dir -DZ3_DIR=$z3_builddir`)
 run(`make`)
